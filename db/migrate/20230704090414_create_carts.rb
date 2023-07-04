9ddc525b11e220842ec8cde8bcd0b8ddae90a08f
Sheila Seruya <Sheilaseruya@gmail.com>
2023-07-04 12:12:21 +0300

    initial boilerplate

---
+++ db/migrate/20230704090414_create_carts.rb
@@ -0,0 +1,13 @@
+class CreateCarts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :carts do |t|
+      t.integer :product_id
+      t.integer :user_id
+      t.integer :price
+      t.integer :total
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
